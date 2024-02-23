983324b4c908df37464fc90af8eb4a61d6b79373
Alexander <stdAlexTikhonov@hotmail.com>
2024-02-24 00:05:01 +0300

    TASK-5: added constants module

--- node/dataset_generator.js
+++ node/dataset_generator.js
@@ -1,17 +1,9 @@
 const draw = require('../common/draw.js');
+const constants = require('../common/constants.js');
 
 const { createCanvas } = require('canvas');
 const canvas = createCanvas(400, 400);
 const ctx = canvas.getContext('2d');
-
-const constants = {};
-
-constants.DATA_DIR = '../data';
-constants.RAW_DIR = constants.DATA_DIR + '/raw';
-constants.DATASET_DIR = constants.DATA_DIR + '/dataset';
-constants.JSON_DIR = constants.DATASET_DIR + '/json';
-constants.IMG_DIR = constants.DATASET_DIR + '/img'; 
-constants.SAMPLES = constants.DATASET_DIR + '/samples.json';
 
 const fs = require('fs');
 const fileNames = fs.readdirSync(constants.RAW_DIR);
