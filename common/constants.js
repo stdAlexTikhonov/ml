eec9dfd8f9370aa4d29585eafd73a14b9fafaffe
Alexander <stdAlexTikhonov@hotmail.com>
2024-02-24 11:50:02 +0300

    TASK-6 added js objects

--- common/constants.js
+++ common/constants.js
@@ -6,5 +6,7 @@ constants.DATASET_DIR = constants.DATA_DIR + '/dataset';
 constants.JSON_DIR = constants.DATASET_DIR + '/json';
 constants.IMG_DIR = constants.DATASET_DIR + '/img'; 
 constants.SAMPLES = constants.DATASET_DIR + '/samples.json';
+constants.JS_OBJECTS = '../common/js_objects';
+constants.SAMPLES_JS = constants.JS_OBJECTS + '/samples.js';
 
 if (module) module.exports = constants;
--- node/dataset_generator.js
+++ node/dataset_generator.js
@@ -30,6 +30,7 @@ fileNames.forEach(fn => {
 });
 
 fs.writeFileSync(constants.SAMPLES, JSON.stringify(samples));
+fs.writeFileSync(constants.SAMPLES_JS, 'const samples = ' + JSON.stringify(samples)) + ';';
 
 function generateImageFile (outfile, paths) {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
