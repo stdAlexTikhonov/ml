44188026958fa58c71c448a18fc130e3251717f1
Alexander <stdAlexTikhonov@hotmail.com>
2024-02-27 21:55:04 +0300

    TASK-8 data sync

--- node/feature_extractor.js
+++ node/feature_extractor.js
@@ -1,5 +1,5 @@
 const constants = require('../common/constants.js');
-const featureFunctions = require('../common/featureFunctions.js');
+const featureFunctions = require('../common/features.js');
 
 const fs = require('fs');
 
@@ -10,13 +10,11 @@ const samples = JSON.parse(fs.readFileSync(constants.SAMPLES));
 for (const sample of samples) {
     const paths = JSON.parse(fs.readFileSync(constants.JSON_DIR + '/' + sample.id + '.json'));
 
-    sample.point = [
-        featureFunctions.getPathCount(paths),
-        featureFunctions.getPointCount(paths)
-    ]
+    const functions = featureFunctions.inUse.map(item => item.function);
+    sample.point = functions.map(f => f(paths));
 }
 
-const featureNames = ['Path count', 'Point count'];
+const featureNames = featureFunctions.inUse.map(f => f.name);
 
 fs.writeFileSync(constants.FEATURES, JSON.stringify({ featureNames, samples }));
 
