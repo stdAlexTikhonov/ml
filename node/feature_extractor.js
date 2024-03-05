77c7381e1bd7b6152882be55e6aa1dd8918f0523
Alexander <stdAlexTikhonov@hotmail.com>
2024-03-05 22:11:13 +0300

    TASK-11: preparing data for evaluation

--- node/feature_extractor.js
+++ node/feature_extractor.js
@@ -15,15 +15,38 @@ for (const sample of samples) {
     sample.point = functions.map(f => f(paths));
 }
 
-const minMax = utils.normalizePoints(
-    samples.map(s => s.point)
-)
-
 const featureNames = featureFunctions.inUse.map(f => f.name);
+
+console.log('Generating splits ...');
+
+const trainingAmount = samples.length * 0.5;
+
+const training = [];
+const testing = [];
+
+for (let i = 0; i < samples.length; i++) {
+    if (i < trainingAmount) training.push(samples[i]);
+    else testing.push(samples[i]);
+}
+
+const minMax = utils.normalizePoints(
+    training.map(s => s.point)
+)
+utils.normalizePoints(
+    testing.map(s => s.point), minMax
+)
 
 fs.writeFileSync(constants.FEATURES, JSON.stringify({ featureNames, samples }));
 
 fs.writeFileSync(constants.FEATURES_JS, `const features = ${JSON.stringify({ featureNames, samples })};`);
+
+fs.writeFileSync(constants.TRAINING, JSON.stringify({ featureNames, samples: training }));
+
+fs.writeFileSync(constants.TRAINING_JS, `const training = ${JSON.stringify({ featureNames, samples: training })};`);
+
+fs.writeFileSync(constants.TESTING, JSON.stringify({ featureNames, samples: testing }));
+
+fs.writeFileSync(constants.TESTING_JS, `const testing = ${JSON.stringify({ featureNames, samples: testing })};`);
 
 fs.writeFileSync(constants.MIN_MAX_JS, `const minMax = ${JSON.stringify(minMax)};`);
 
